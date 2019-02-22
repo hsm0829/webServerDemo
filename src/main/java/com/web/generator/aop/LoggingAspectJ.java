f88e03566059c1b73881c3485b805f0f450456a1
hsm0829 <843856964@qq.com>
2019-02-22 17:57:17 +0800

    aop后置通知

--- src/main/java/com/web/generator/aop/LoggingAspectJ.java
+++ src/main/java/com/web/generator/aop/LoggingAspectJ.java
@@ -1,6 +1,7 @@
 package com.web.generator.aop;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -23,4 +24,15 @@ public class LoggingAspectJ {
         String methodName = joinPoint.getSignature().getName();
         System.out.println("the method "+methodName+" begins with "+ Arrays.asList(joinPoint.getArgs()));
     }
+    /**
+     * 后置通知：在目标方法执行后，无论是否异常，都会执行
+     * 在后置通知中，还不能访问目标方法执行的结果
+     * @param joinPoint
+     */
+    @After("execution(* com.web.generator.aop.ArithmeticCalculatorI.*(..))")
+    public void afterMethod(JoinPoint joinPoint){
+        String methodName = joinPoint.getSignature().getName();
+        System.out.println("the method "+methodName+" ends with "+ Arrays.asList(joinPoint.getArgs()));
+    }
 }
+
