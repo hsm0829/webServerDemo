d7bf6a52498716658c7f89d884bf3b70028f4fe5
hsm0829 <843856964@qq.com>
2019-02-25 16:53:09 +0800

    aop 声明切入点

--- src/main/java/com/web/generator/aop/LoggingAspectJ.java
+++ src/main/java/com/web/generator/aop/LoggingAspectJ.java
@@ -20,72 +20,72 @@ public class LoggingAspectJ {
      * 前置通知
      * @param joinPoint
      */
-    @Before("execution(* com.web.generator.aop.ArithmeticCalculatorI.*(..))")
+    @Before("ValidationAspectJ.declarationPointCutExpression()")
     public void beforeMethod(JoinPoint joinPoint){
         String methodName = joinPoint.getSignature().getName();
         System.out.println("the method "+methodName+" begins with "+ Arrays.asList(joinPoint.getArgs()));
     }
-//    /**
-//     * 后置通知：在目标方法执行后，无论是否异常，都会执行
-//     * 在后置通知中，还不能访问目标方法执行的结果
-//     * @param joinPoint
-//     */
-//    @After("execution(* com.web.generator.aop.ArithmeticCalculatorI.*(..))")
-//    public void afterMethod(JoinPoint joinPoint){
-//        String methodName = joinPoint.getSignature().getName();
-//        System.out.println("the method "+methodName+" ends with "+ Arrays.asList(joinPoint.getArgs()));
-//    }
-//
-//    /**
-//     * 返回通知：目标方法正常结束后执行
-//     * 在返回通知中，可以访问到目标方法的返回值
-//     * @param joinPoint
-//     * @param result
-//     */
-//    @AfterReturning(value = "execution(* com.web.generator.aop.ArithmeticCalculatorI.*(..))" , returning = "result")
-//    public void afterReturnMethod(JoinPoint joinPoint,Object result){
-//        String methodName = joinPoint.getSignature().getName();
-//        System.out.println("the method "+methodName+" result ="+ result);
-//    }
-//
-//    /**
-//     * 异常通知：在目标方法出现异常时，执行的代码
-//     * 可以访问到异常对象，且可以指定在特定异常时，再执行异常通知代码
-//     * @param joinPoint
-//     * @param e
-//     */
-//    @AfterThrowing(value = "execution(* com.web.generator.aop.ArithmeticCalculatorI.*(..))",throwing = "e")
-//    public void throwMethod(JoinPoint joinPoint,Exception e){
-//        String methodName = joinPoint.getSignature().getName();
-//        System.out.println("the method "+methodName+" exception ="+ e);
-//    }
+    /**
+     * 后置通知：在目标方法执行后，无论是否异常，都会执行
+     * 在后置通知中，还不能访问目标方法执行的结果
+     * @param joinPoint
+     */
+    @After("ValidationAspectJ.declarationPointCutExpression()")
+    public void afterMethod(JoinPoint joinPoint){
+        String methodName = joinPoint.getSignature().getName();
+        System.out.println("the method "+methodName+" ends with "+ Arrays.asList(joinPoint.getArgs()));
+    }
 
-//    /**
-//     * 环绕通知：需要携带 ProceedingJoinPoint 类型的参数
-//     * 环绕通知类似于动态代理的全过程：ProceedingJoinPoint 类型的参数可以决定是否执行目标方法
-//     * 且环绕通知必须有返回值，即目标方法的返回值
-//     * @param pjd
-//     * @return
-//     */
-//    @Around("execution(* com.web.generator.aop.ArithmeticCalculatorI.*(..))")
-//    public Object aroundMehtod(ProceedingJoinPoint pjd){
-//        Object result=null;
-//        String name = pjd.getSignature().getName();
-//        try {
-//            //前置通知
-//            System.out.println("The method name "+name+" begins ...");
-//            result=pjd.proceed();
-//            System.out.println("result = "+result);
-//            //后置通知
-//            System.out.println("The method name "+name+" ends ...");
-//            //返回通知
-//            System.out.println("The method name "+name+" result "+result);
-//        } catch (Throwable throwable) {
-//            //异常通知
-//            System.out.println("The method exception "+throwable);
-//        }
-//        return result;
-//    }
+    /**
+     * 返回通知：目标方法正常结束后执行
+     * 在返回通知中，可以访问到目标方法的返回值
+     * @param joinPoint
+     * @param result
+     */
+    @AfterReturning(value = "ValidationAspectJ.declarationPointCutExpression()" , returning = "result")
+    public void afterReturnMethod(JoinPoint joinPoint,Object result){
+        String methodName = joinPoint.getSignature().getName();
+        System.out.println("the method "+methodName+" result ="+ result);
+    }
+
+    /**
+     * 异常通知：在目标方法出现异常时，执行的代码
+     * 可以访问到异常对象，且可以指定在特定异常时，再执行异常通知代码
+     * @param joinPoint
+     * @param e
+     */
+    @AfterThrowing(value = "ValidationAspectJ.declarationPointCutExpression()",throwing = "e")
+    public void throwMethod(JoinPoint joinPoint,Exception e){
+        String methodName = joinPoint.getSignature().getName();
+        System.out.println("the method "+methodName+" exception ="+ e);
+    }
+
+    /**
+     * 环绕通知：需要携带 ProceedingJoinPoint 类型的参数
+     * 环绕通知类似于动态代理的全过程：ProceedingJoinPoint 类型的参数可以决定是否执行目标方法
+     * 且环绕通知必须有返回值，即目标方法的返回值
+     * @param pjd
+     * @return
+     */
+    @Around("ValidationAspectJ.declarationPointCutExpression()")
+    public Object aroundMehtod(ProceedingJoinPoint pjd){
+        Object result=null;
+        String name = pjd.getSignature().getName();
+        try {
+            //前置通知
+            System.out.println("The method name "+name+" begins ...");
+            result=pjd.proceed();
+            System.out.println("result = "+result);
+            //后置通知
+            System.out.println("The method name "+name+" ends ...");
+            //返回通知
+            System.out.println("The method name "+name+" result "+result);
+        } catch (Throwable throwable) {
+            //异常通知
+            System.out.println("The method exception "+throwable);
+        }
+        return result;
+    }
 
 }
 
--- src/main/java/com/web/generator/aop/ValidationAspectJ.java
+++ src/main/java/com/web/generator/aop/ValidationAspectJ.java
@@ -3,6 +3,7 @@ package com.web.generator.aop;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -16,7 +17,15 @@ import java.util.Arrays;
 @Component
 public class ValidationAspectJ {
 
-    @Before("execution(* com.web.generator.aop.ArithmeticCalculatorI.*(..))")
+    /**
+     * 声明切入点表达式，一般的，该方法不在添加其他代码
+     * 其他通知，引用该切点，只用引用该切点的方法名，其他切面引用该切点，需 类名.方法名，不同包需： 包名.类名.方法名
+     *
+     */
+    @Pointcut("execution(* com.web.generator.aop.ArithmeticCalculatorI.*(..))")
+    public void declarationPointCutExpression(){}
+
+    @Before("declarationPointCutExpression()")
     public void validaMethod(JoinPoint joinPoint){
         String name = joinPoint.getSignature().getName();
         System.out.println("the method "+name+" valida with "+ Arrays.asList(joinPoint.getArgs()));
